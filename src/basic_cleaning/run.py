c5be91d6e3b171e3b8972ec4f97cab99102bdacb
fucheng96 <fuchengliew@gmail.com>
2024-01-03 15:49:29 +0800

    Added new files to eda & basic_cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -2,84 +2,111 @@
 """
 Performs basic data cleaning and save the results in Weights & Biases
 """
+# Imort libraries
 import argparse
 import logging
+import pandas as pd
 import wandb
 
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+# Logging configurations
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)-15s %(message)s"
+)
 logger = logging.getLogger()
 
 
 def go(args):
-
+    """
+    Performs basic data cleaning and save the results in Weights & Biases
+    """
+    # Initiate run
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    # Download input artifact
+    input_artifact_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(input_artifact_path)
+    logger.info("basic_cleaning: input_artifact downloaded")
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # Drop outliers
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    logger.info("basic_cleaning: Outliers dropped")
+
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    logger.info("basic_cleaning: Converted last_review to datetime")
+
+    # Save the cleaned dataset
+    file_name = "clean_sample.csv"
+    df.to_csv(file_name, index=False)
+    logger.info("basic_cleaning: output_artifact saved")
+
+    # Add saved artifact to wandb
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(file_name)
+
+    # Log artifact to wandb
+    run.log_artifact(artifact)
+    logger.info("basic_cleaning: Artifact logged")
 
 
 if __name__ == "__main__":
+    # Instantiate parser
+    parser = argparse.ArgumentParser(
+        description="This step preprocesses the data"
+    )
 
-    parser = argparse.ArgumentParser(description="This step preprocesses the data")
-
-
+    # Add arguments into parser
     parser.add_argument(
         "--input_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Name of the input_artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
-    )
-
-    parser.add_argument(
-        "--", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Name of output_artifact to be created and saved in wandb",
         required=True
     )
 
     parser.add_argument(
         "--output_type", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Type of output_artifact to create",
         required=True
     )
 
     parser.add_argument(
         "--output_description", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Description of output_artifact",
         required=True
     )
 
     parser.add_argument(
         "--min_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=float,
+        help="Minimum rental price in data validation",
         required=True
     )
 
     parser.add_argument(
         "--max_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=float,
+        help="Maximum rental price in data validation",
         required=True
     )
 
-
     args = parser.parse_args()
 
+    # Run go function with arguments in parser
     go(args)
